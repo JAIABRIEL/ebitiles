2b8e864c505f5bf39cd1f625ee03189e52266ae4
Gabriel Jann <mail@jannga.org>
2024-02-26 15:01:56 +0100

    Add concept for interfaces

---
+++ types.go
@@ -0,0 +1,18 @@
+// Package tilemap docstring
+package tilemap
+
+// Quad is a basic quad tree interface
+type Quad interface {
+	Redraw() TileImage
+	GetTile(int, int) *Tile
+}
+
+// Image is a placeholder for your own draw function
+type TileImage interface {
+	// DrawAt draws another image at a position on this image.
+	// Be aware that position is given in tiles.
+	DrawAt(TileImage, int, int)
+
+	// New resets this image to a new image of a given size.
+	New(int)
+}
