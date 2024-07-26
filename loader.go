1edb97e8426ce8378b0857a01d9c3c4a8ddc638e
Gabriel Jann <mail@jannga.org>
2024-07-26 12:04:08 +0200

    Add Draw function

--- loader.go
+++ loader.go
@@ -1,5 +1,11 @@
 package ebitiles
 
+import (
+	"math"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
 type Loader struct {
 	// TileMap represents the TileMap from which chunks are provided.
 	TileMap *TileMap
@@ -9,7 +15,28 @@ type Loader struct {
 	ChunkPosY int
 	Level     ChunkLevel
 	Radius    int
+
+	chunkSize int
+}
+
+func (l *Loader) Init() {
+	l.chunkSize = int(math.Pow(2, float64(l.Level)))
 }
 
 func (l *Loader) Update(x, y int) {
+	l.ChunkPosX = x / l.chunkSize
+	l.ChunkPosY = y / l.chunkSize
+}
+
+func (l *Loader) Draw(img *ebiten.Image) {
+	x := l.ChunkPosX - l.Radius
+
+	for x <= l.ChunkPosX {
+		y := l.ChunkPosY - l.Radius
+		for y <= l.ChunkPosY {
+			l.TileMap.TestQuadByLevel(l.Level, x, y).Draw(img)
+			y++
+		}
+		x++
+	}
 }
